f35c11d5979ce16c425ad0b70c478d8ca427d1d3
neverbeenthisweeb <rf.ramadhana@gmail.com>
2020-11-01 07:05:55 +0700

    Implement basic functionalities
    Implement components import/export, data, methods, and props.

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <!-- NOTE: Prevent default behavior -->
+        <form @submit.prevent="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+            <input type="submit" name="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+import {v4 as uuidv4} from 'uuid';
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            const newTodo = {
+                id: uuidv4(),
+                title: this.title,
+                completed: false
+            }
+            // send up to parents
+            this.$emit('add-todo', newTodo);
+            // clear form
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
